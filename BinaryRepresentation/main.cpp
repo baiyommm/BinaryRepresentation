dc06d90479f3cc98eed923cd6dfacbd06099f46d
Chatchapong Thuaypha <chatchapong1@windowslive.com>
2024-09-23 01:55:50 -0700

    Completed program to convert the user input number to a 32-bit  binary representation using bitwise operations.

--- BinaryRepresentation/main.cpp
+++ BinaryRepresentation/main.cpp
@@ -1,14 +1,37 @@
-//
-//  main.cpp
-//  BinaryRepresentation
-//
-//  Created by Chatchapong Thuaypha on 9/23/24.
-//
-
 #include <iostream>
+using namespace std;
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+void bin(long n) {
+    long i;
+    cout << "0"; // To indicate a binary number
+    
+    // For loop to iterate over 32 bits
+    for (i = 1L << 31; i > 0; i = i / 2) {
+        if ((n & i) != 0) {
+            cout << "1";
+        } else {
+            cout << "0";
+        }
+    }
+    cout << endl;
+}
+
+int main() {
+    
+    long num;
+
+    // Takes input from the user
+    cout << "Enter a positive integer: ";
+    cin >> num;
+
+    // Checks if the input is a positive number
+    if (num < 0) {
+        cout << "Please enter a positive integer." << endl;
+        return 1;
+    }
+
+    // bin() function to display the results of binary representation
+    bin(num);
+
     return 0;
 }
